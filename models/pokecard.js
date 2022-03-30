8bdd20e3c356cc5a7e7ee83ea7b64e658f656ecb
hsuzach <hsuzach@gmail.com>
2022-03-30 11:53:03 -0700

    pokecard model

---
+++ models/pokecard.js
@@ -0,0 +1,83 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize')
+// import our database connection from config.js
+const sequelize = require('../config/connection')
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class PokeCard extends Model {}
+
+PokeCard.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    hp: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    image: {
+      type: DataTypes.BLOB,
+      allowNull: false
+    },
+    backgroundimage: {
+      type: DataTypes.BLOB,
+      allowNull: false
+    },
+    attack1name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack1damage: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack1description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack2name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack2damage: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    attack2description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id:{
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'PokeCard'
+  }
+)
+
+module.exports = PokeCard
